12c6ec9065b3935f234c5d6838de44e7b2879aa9
Nathalia <nathaliavieiramota@gmail.com>
2024-01-30 17:21:24 +0100

    Create ex1.sql

---
+++ Queries/ex1.sql
@@ -0,0 +1,14 @@
+/*Write a select query to return:
+	last name
+	email
+	department name
+For employees with salaries greater than $120,000.*/
+
+select 
+	e.last_name,
+	e.email,
+	cd.department_name
+from data_sci.employees e
+join data_sci.company_departments cd
+on e.department_id = cd.id
+where e.salary > 120000;
